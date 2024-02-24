62ed3fce8c7d59c790880434e6c1dcbbf00b6203
Owen Fujii <dalekrebel@gmail.com>
2024-02-23 18:30:28 -0700

    Add project files.

---
+++ ShuffleTheDeck.vb
@@ -0,0 +1,45 @@
+﻿Module ShuffleTheDeck
+
+    'TODO
+    '[] Start Deck
+    '[] Draw Card
+    '[] Track Card
+    '[] Display Card #
+    '[] Draw Card 2
+    '[] Repeat but no repeat cards can be displayed
+    '[] Restart or shuffle at end
+
+
+    Sub Main()
+            Dim userInput As String
+            Dim tracker(14, 4) As Boolean ' each letter had 15 unique #
+            Dim ballsDrawn As Integer = 0
+            Dim message As String = "Hello There!"
+        Shuffler.NewGame(tracker)
+        Do
+                Console.Clear()
+            Shuffler.Display(tracker)
+            Console.WriteLine(message)
+                userInput = Console.ReadLine()
+                If userInput = "n" Then
+                Shuffler.NewGame(tracker)
+            Else
+                    If ballsDrawn >= 75 Then
+                        message = "All balls have been drawn"
+                        Exit Do
+
+                    Else
+                    Shuffler.DrawBall(tracker)
+                    ballsDrawn += 1
+                        message = "Press enter to draw, N for new game, or Q for quit"
+                    End If
+                End If
+
+
+            Loop Until userInput = "q"
+            Console.WriteLine("Have a Nice Day!")
+            Console.Read()
+        End Sub
+
+
+End Module
---
+++ Shuffler.vb
@@ -0,0 +1,53 @@
+﻿Module Shuffler
+    Sub NewGame(ByRef tracker(,) As Boolean)
+        Console.Clear()
+        ReDim tracker(14, 4)
+    End Sub
+
+    Sub DrawBall(ByRef tracker(,) As Boolean)
+        Dim currentLetter%, currentNumber%
+        ' loop until we get a ball that has not been drawn yet
+        ' update tracker marking the ball
+        Do
+            ' potential endless loop here
+            currentNumber = RandomNumberZero(14)
+            currentLetter = RandomNumberZero(4)
+        Loop While tracker(currentNumber, currentLetter)
+
+        tracker(currentNumber, currentLetter) = True
+
+    End Sub
+
+    Private Function RandomNumberZero(max%) As Integer
+        Dim _randomNumber As Integer
+        Randomize()
+        _randomNumber = CInt(Math.Floor(Rnd() * (max + 1)))
+        Return _randomNumber
+    End Function
+
+    Sub Display(tracker(,) As Boolean)
+
+        Dim temp(14, 4) As Boolean
+
+        temp = tracker
+        Dim header() = {"B", "I", "N", "G", "O"}
+        Dim math As Integer
+        For Each letter In header
+            Console.Write(letter.PadLeft(2).PadRight(4))
+        Next
+        Console.WriteLine()
+        'temp(7, 3) = True
+        'temp(3, 4) = True
+        For row = 0 To 14
+            For column = 0 To 4
+                If temp(row, column) Then
+                    math = (column * 15) + (row + 1)
+                    Console.Write($"{math}")
+                Else
+                    Console.Write("X")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+End Module
